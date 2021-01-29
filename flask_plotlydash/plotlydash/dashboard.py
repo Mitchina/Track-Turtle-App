642790f78f070e42d9ae16ad67ee06e18bc110da
Juunicacio <juhnicacio.bio@gmail.com>
2021-01-29 23:41:09 +0100

    Refactoring: code cleaned with functions

--- flask_plotlydash/plotlydash/dashboard.py
+++ flask_plotlydash/plotlydash/dashboard.py
@@ -30,6 +30,144 @@ import plotly.express as px # for the 2nd graph
 
 
 
+# Function to return all the variables I need from the Depth data
+# Layer values (float), minimum Layer value (float), maximum Layer value (float), Layer values in Percentage
+def loadLayerData(LayerColumn,JsonDepthData):
+
+    # Looping through Layer -----------------------------
+    jlayerDepths = []
+    for i in JsonDepthData['features']:
+        layer = i['properties'][LayerColumn]
+        jlayerDepths.append(layer)
+        
+    # Assigning variables for max and min float values in layer percentage
+    jminPercLay = min(feature["properties"][LayerColumn] for feature in JsonDepthData['features'])
+    jmaxPercLay = max(feature["properties"][LayerColumn] for feature in JsonDepthData['features'])
+
+    # Converted Decimals of Layer to Percentage just to use in the hovertext, for visualization, it don't work with the markers
+    jlayerDepthsInPercentage = []
+    for i in JsonDepthData['features']:
+        intNum = i['properties'][LayerColumn]*100
+        percSymbol = '{:.2f}%'.format( intNum )
+        jlayerDepthsInPercentage.append(percSymbol)
+
+    return jlayerDepths,jminPercLay,jmaxPercLay,jlayerDepthsInPercentage
+
+# Function to make an histogram data for Layer --------------------------------------------------------
+def generateHistogramGraph(LayerValueInPercentage, LayerNumber):
+    
+    jgohistlayer = [go.Histogram(x=LayerValueInPercentage, 
+                         opacity=0.4,
+                         marker=dict(color='orange'))]
+    jlayout = go.Layout(barmode='overlay',
+                                 title='Layer {LayerNumber} histogram',
+                                 yaxis_title='Count',
+                                 xaxis_title='Layer {LayerNumber} occurrence in %') # or 'Percentage (%) of occurrence in Layer'
+    jfig = go.Figure(
+            {"data": jgohistlayer,
+             "layout": jlayout})
+
+    return jfig
+
+# Function to make Go Map for Layer  -------------------------------------------------------------------
+def generateGeoMap (jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths, 
+    jmaxPercLay, jminPercLay, jlayerDepthsInPercentage, LayerNumber):
+    
+    jgomaptraceLayer = go.Figure(go.Scattermapbox(
+                                    lat=jyDegreeGps,
+                                    lon=jxDegreeGps,
+                                    name = 'GPS Data',
+                                    mode="markers+lines",
+                                    marker = {'size': 8, 'color': 'yellow'}, # changed the size
+                                    text = jacquisitionGps,
+                                    hoverinfo='text'
+                                ))
+    jgomaptraceLayer.add_trace(go.Scattermapbox(
+                                    lat=jyDegreeDepth,
+                                    lon=jxDegreeDepth,
+                                    name = 'Depth data from 0 to -5 meters deep', #############
+                                    mode = "markers+lines",
+                                    text = jlayerDepths,
+                                    marker = {        
+                                        'colorscale':[[0, 'green'], [1, 'rgb(0, 0, 255)']],
+                                        'color': jlayerDepths,
+                                        'cmax':jmaxPercLay,
+                                        'cmin':jminPercLay,
+                                        'size': jlayerDepths,
+                                        'sizemin':0.1,
+                                        'sizemode': 'area',
+                                        'sizeref': jmaxPercLay / 6 **2,
+                                        'showscale':True,
+                                        'colorbar': {
+                                            'title': 'Layer {LayerNumber} occurrence in %', # including a colorbar
+                                            'titleside':'top',
+                                            'x': 0,
+                                            'y': 0.5,
+                                            'tickformat': ".0%", # Formating tick labels to percentage on color bar
+                                            'tickfont': {
+                                                'color': '#000000',
+                                                'family':"Open Sans",
+                                                'size': 14
+                                            }
+                                        }
+                                    },   
+                                    hoverinfo='text',
+                                    hovertext = jlayerDepthsInPercentage,  #100 * x), #(lambda x: '{0:1.2f}%'.format(x)#{:. n%} 
+                                    opacity = 1
+                                ))
+    jgomaptraceLayer.update_layout(
+                margin ={'l':0,'t':0,'b':0,'r':0},
+                showlegend=False, # change if you want to see the legend *
+                mapbox = {        
+                    'style': "stamen-terrain",
+                    'center': {'lon': 10, 'lat': 37},
+                    'zoom': 5})
+
+    return jgomaptraceLayer
+
+def addScatterGraphTrace(jgoscattermap,jacquisitionDepth,jlayerDepthsInPercentage,LayerNumber,UpperDepthLayerRange,LowerDepthLayerRange):
+    jgoscattermap.add_trace(
+        go.Scatter(
+            x = jacquisitionDepth,
+            y = jlayerDepthsInPercentage,
+            name=f'Layer {LayerNumber}: between {UpperDepthLayerRange} to {LowerDepthLayerRange} meters deep',
+            showlegend=True,
+            marker = dict(
+                line = dict(
+                width = 1,
+                color = 'DarkSlateGrey')
+            )
+        )
+    )
+
+# Making Go Scatter for Layer 1 -------------------------------------------------------------------
+def generateScatterGraph():
+
+    jgoscattermapLayer = go.Figure()
+    #for vender in temp.columns:
+    
+    jgoscattermapLayer.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1h", step="hour", stepmode="backward"),
+                dict(count=1, label="1d", step="day", stepmode="backward"),
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=2, label="2m", step="month", stepmode="backward"),
+                dict(count=3, label="3m", step="month", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+
+    jgoscattermapLayer.update_layout(title='Depth Occurrence',
+                        legend = {'orientation': 'h', 'x': 0.1 ,'y':1.4},
+                        showlegend=True, # change if you want to see the legend *
+                        xaxis={'title': 'Datetime'},
+                        yaxis={'title': 'Occurrence in %'}
+    )
+
+    return jgoscattermapLayer
 
 # Flask app into Dash as server
 def init_dashboard(server): # or create_dashboard
@@ -154,39 +292,67 @@ def init_dashboard(server): # or create_dashboard
         aquisGps = i['properties']['Acquisitio']
         jacquisitionGps.append(aquisGps)
 
-    # Looping through Layer 1 -----------------------------
+    """ ###### CREATE A FUNCTION THAT RUNS ALL OF THIS BLOCK FOR EACH LAYER 1-10 ##################
+    # Looping through Layer 1 -----------------
     jlayer1Depths = []
     for i in jdata_depthPointsDegree['features']:
         layer1 = i['properties']['Layer 1 Pe']
-        jlayer1Depths.append(layer1)
-
-    # Assigning variables for max and min float values in layer 1 percentage
+        jlayer1Depths.append(layer1)    
+    
+    # Assigning variables for max and min float values in layer 1 percentage --------------------
     jminPercLay1 = min(feature["properties"]['Layer 1 Pe'] for feature in jdata_depthPointsDegree['features'])
-    jmaxPercLay1 = max(feature["properties"]['Layer 1 Pe'] for feature in jdata_depthPointsDegree['features'])
- 
-    # Converted Decimals to Percentage just to use in the hovertext, for visualization, it don't work with the markers
+    jmaxPercLay1 = max(feature["properties"]['Layer 1 Pe'] for feature in jdata_depthPointsDegree['features'])    
+    
+    # Converted Decimals of Layer 1 to Percentage just to use in the hovertext, for visualization, it don't work with the markers
     jlayer1DepthsInPercentage = []
     for i in jdata_depthPointsDegree['features']:
-        intNum = i['properties']['Layer 1 Pe']*100
-        percSymbol = '{:.2f}%'.format( intNum )
-        jlayer1DepthsInPercentage.append(percSymbol)
-     
+        intNum_L1 = i['properties']['Layer 1 Pe']*100
+        percSymbol_L1 = '{:.2f}%'.format( intNum_L1 )
+        jlayer1DepthsInPercentage.append(percSymbol_L1)
+    """ ###########################################################################################
 
+    # Call function to return all the variables I need from the Depth data
+    # Layer values (float), minimum Layer value (float), maximum Layer value (float), Layer values in Percentage
+    jlayerDepths1,jminPercLay1,jmaxPercLay1,jlayerDepthsInPercentage1 = loadLayerData('Layer 1 Pe',jdata_depthPointsDegree)
+    jlayerDepths2,jminPercLay2,jmaxPercLay2,jlayerDepthsInPercentage2 = loadLayerData('Layer 2 Pe',jdata_depthPointsDegree)
+    jlayerDepths3,jminPercLay3,jmaxPercLay3,jlayerDepthsInPercentage3 = loadLayerData('Layer 3 Pe',jdata_depthPointsDegree)
+    jlayerDepths4,jminPercLay4,jmaxPercLay4,jlayerDepthsInPercentage4 = loadLayerData('Layer 4 Pe',jdata_depthPointsDegree)
+    jlayerDepths5,jminPercLay5,jmaxPercLay5,jlayerDepthsInPercentage5 = loadLayerData('Layer 5 Pe',jdata_depthPointsDegree)
+    jlayerDepths6,jminPercLay6,jmaxPercLay6,jlayerDepthsInPercentage6 = loadLayerData('Layer 6 Pe',jdata_depthPointsDegree)
+    jlayerDepths7,jminPercLay7,jmaxPercLay7,jlayerDepthsInPercentage7 = loadLayerData('Layer 7 Pe',jdata_depthPointsDegree)
+    jlayerDepths8,jminPercLay8,jmaxPercLay8,jlayerDepthsInPercentage8 = loadLayerData('Layer 8 Pe',jdata_depthPointsDegree)
+    jlayerDepths9,jminPercLay9,jmaxPercLay9,jlayerDepthsInPercentage9 = loadLayerData('Layer 9 Pe',jdata_depthPointsDegree)
+    jlayerDepths10,jminPercLay10,jmaxPercLay10,jlayerDepthsInPercentage10 = loadLayerData('Layer 10 P',jdata_depthPointsDegree)
+
+
+    """
     # Making an histogram data for layer 1 --------------------------------------------------------
-    jgohistlayer1 = [go.Histogram(x=jlayer1DepthsInPercentage, #x = layer1Depths, #y=layer1Depths,
+    jgohistlayer1 = [go.Histogram(x=jlayerDepthsInPercentage1, #x = layerDepths1, #y=layerDepths1,
                          opacity=0.4,
                          marker=dict(color='orange'))]
-    jlayout = go.Layout(barmode='overlay',
+    jlayout1 = go.Layout(barmode='overlay',
                                  title='Layer 1 histogram',
                                  yaxis_title='Count',
                                  xaxis_title='Layer 1 occurrence in %') # or 'Percentage (%) of occurrence in Layer 1'
-    jfig = go.Figure(
+    jfig1 = go.Figure(
             {"data": jgohistlayer1,
-             "layout": jlayout})
-    #py.iplot(jfig, filename='Layer 1 histogram') --------- using when debugging in jupyter notebook
+             "layout": jlayout1})
+    #py.iplot(jfig1, filename='Layer 1 histogram') --------- using when debugging in jupyter notebook
+    """
+    jfig1 = generateHistogramGraph(jlayerDepthsInPercentage1, 1)
+    jfig2 = generateHistogramGraph(jlayerDepthsInPercentage2, 2)
+    jfig3 = generateHistogramGraph(jlayerDepthsInPercentage3, 3)
+    jfig4 = generateHistogramGraph(jlayerDepthsInPercentage4, 4)
+    jfig5 = generateHistogramGraph(jlayerDepthsInPercentage5, 5)
+    jfig6 = generateHistogramGraph(jlayerDepthsInPercentage6, 6)
+    jfig7 = generateHistogramGraph(jlayerDepthsInPercentage7, 7)
+    jfig8 = generateHistogramGraph(jlayerDepthsInPercentage8, 8)
+    jfig9 = generateHistogramGraph(jlayerDepthsInPercentage9, 9)
+    jfig10 = generateHistogramGraph(jlayerDepthsInPercentage10, 10)
 
 
-    # Making Box Plot with plotly.express - didn't work
+    # Box Plot with plotly.express - didn't work
+    """
     # Making Go Map for Layer 1 -------------------------------------------------------------------
     jgomaptraceLayer1 = go.Figure(go.Scattermapbox(
                                     lat=jyDegreeGps,
@@ -202,13 +368,13 @@ def init_dashboard(server): # or create_dashboard
                                     lon=jxDegreeDepth,
                                     name = 'Depth data from 0 to -5 meters deep',
                                     mode = "markers+lines",
-                                    text = jlayer1Depths,
+                                    text = jlayerDepths1,
                                     marker = {        
                                         'colorscale':[[0, 'green'], [1, 'rgb(0, 0, 255)']],
-                                        'color': jlayer1Depths,
+                                        'color': jlayerDepths1,
                                         'cmax':jmaxPercLay1,
                                         'cmin':jminPercLay1,
-                                        'size': jlayer1Depths,
+                                        'size': jlayerDepths1,
                                         'sizemin':0.1,
                                         'sizemode': 'area',
                                         'sizeref': jmaxPercLay1 / 6 **2,
@@ -227,7 +393,7 @@ def init_dashboard(server): # or create_dashboard
                                         }
                                     },   
                                     hoverinfo='text',
-                                    hovertext = jlayer1DepthsInPercentage,  #100 * x), #(lambda x: '{0:1.2f}%'.format(x)#{:. n%} 
+                                    hovertext = jlayerDepthsInPercentage1,  #100 * x), #(lambda x: '{0:1.2f}%'.format(x)#{:. n%} 
                                     opacity = 1
                                 ))
     jgomaptraceLayer1.update_layout(
@@ -241,13 +407,36 @@ def init_dashboard(server): # or create_dashboard
 
     # the "fig" is jgomaptraceLayer1
 
+    """
+    jgomaptraceLayer1 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths1, 
+    jmaxPercLay1, jminPercLay1, jlayerDepthsInPercentage1, 1)
+    jgomaptraceLayer2 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths2, 
+    jmaxPercLay2, jminPercLay2, jlayerDepthsInPercentage2, 2)
+    jgomaptraceLayer3 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths3, 
+    jmaxPercLay3, jminPercLay3, jlayerDepthsInPercentage3, 3)
+    jgomaptraceLayer4 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths4, 
+    jmaxPercLay4, jminPercLay4, jlayerDepthsInPercentage4, 4)
+    jgomaptraceLayer5 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths5, 
+    jmaxPercLay5, jminPercLay5, jlayerDepthsInPercentage5, 5)
+    jgomaptraceLayer6 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths6, 
+    jmaxPercLay6, jminPercLay6, jlayerDepthsInPercentage6, 6)
+    jgomaptraceLayer7 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths7, 
+    jmaxPercLay7, jminPercLay7, jlayerDepthsInPercentage7, 7)
+    jgomaptraceLayer8 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths8, 
+    jmaxPercLay8, jminPercLay8, jlayerDepthsInPercentage8, 8)
+    jgomaptraceLayer9 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths9, 
+    jmaxPercLay9, jminPercLay9, jlayerDepthsInPercentage9, 9)
+    jgomaptraceLayer10 = generateGeoMap(jyDegreeGps, jxDegreeGps, jacquisitionGps, jyDegreeDepth, jxDegreeDepth, jlayerDepths10, 
+    jmaxPercLay10, jminPercLay10, jlayerDepthsInPercentage10, 10)
+
+    """
     # Making Go Scatter for Layer 1 -------------------------------------------------------------------
-    jgoscattermap = go.Figure()
+    jgoscattermapLayer1 = go.Figure()
     #for vender in temp.columns:
-    jgoscattermap.add_trace(
+    jgoscattermapLayer1.add_trace(
         go.Scatter(
             x = jacquisitionDepth,
-            y = jlayer1DepthsInPercentage,
+            y = jlayerDepthsInPercentage1,
             name='Layer 1: between 0 to -5 meters deep',
             showlegend=True,
             marker = dict(
@@ -258,7 +447,7 @@ def init_dashboard(server): # or create_dashboard
         )
     )
 
-    jgoscattermap.update_xaxes(
+    jgoscattermapLayer1.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
             buttons=list([
@@ -272,16 +461,26 @@ def init_dashboard(server): # or create_dashboard
         )
     )
 
-    jgoscattermap.update_layout(title='Depth Occurrence',
+    jgoscattermapLayer1.update_layout(title='Depth Occurrence',
                         legend = {'orientation': 'h', 'x': 0.1 ,'y':1.4},
+                        showlegend=False, # change if you want to see the legend *
                         xaxis={'title': 'Datetime'},
                         yaxis={'title': 'Occurrence in %'}
     )
 
-    # the "fig" is jgoscattermap
-
-
-
+    # the "fig" is jgoscattermapLayer1
+    """
+    jgoscatterGraph = generateScatterGraph()
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage1,1,0,-5)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage2,2,-6,-10)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage3,3,-11,-20)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage4,4,-21,-30)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage5,5,-31,-40)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage6,6,-41,-50)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage7,7,-51,-70)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage8,8,-71,-90)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage9,9,-91,-110)
+    addScatterGraphTrace(jgoscatterGraph,jacquisitionDepth,jlayerDepthsInPercentage10,10,-111,-4095)
     ######################################################## END DATA GRAPHS ######################
 
     #dash_app.layout = html.Div ([
@@ -424,7 +623,7 @@ def init_dashboard(server): # or create_dashboard
                 dbc.Col(html.Div(className= 'graph_graph col-sm', children=[ #calling histogram graph layer 1 ------------------                
                         dcc.Graph(
                             id='hist_graph',
-                            figure=jfig
+                            figure=jfig1 #################################### callback 
                         ) # ------------------- end histogram)
                     ]),
                 #html.Div(className= 'clear'),
@@ -464,7 +663,7 @@ def init_dashboard(server): # or create_dashboard
                         html.Div(children= 'Depth Occurrence in %'),
                         dcc.Graph( # calling Scatter graph layer 1 -------------------------------
                                 id='scatter_graph',
-                                figure=jgoscattermap
+                                figure=jgoscatterGraph
                                 ) # ------------------- end Scatter
                     ])
                 )
